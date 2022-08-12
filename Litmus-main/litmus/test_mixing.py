ff0c6189025b3c879fe4dd18f58128a65b9fc66b
Khooshrin Aspi Pithawalla <88196065+Khooshrin@users.noreply.github.com>
2022-08-13 00:26:21 +0530

    Add files via upload

---
+++ Litmus-main/litmus/test_mixing.py
@@ -0,0 +1,23 @@
+import pytest
+import litmus_mixing
+
+def test_suggestions():
+
+	args = litmus_mixing.parse_args([
+	    "xlmr", 
+	    "--load_state", "frontend/Predictivev2/data_xlmr_udpos.pkl",
+	    "--common_scaling",
+	    "--error_method", "kfold",
+	    "--training_algorithm", "xgboost",
+	    "--mode", "suggestions",
+	    "--data_sizes", "ar:5000,ru:5000",
+	    "--suggestions_targets", "en,fr,de,hi,ta,zh,ja",
+	    "--suggestions_budget", "10000"
+	])
+	res = litmus_mixing.litmus_main(args)
+	#print(res)
+	assert ("suggestions" in res)
+	assert (len(res["suggestions"]["augments"]) > 0)
+
+	for each in res:
+		print(each)
